957f30c5ecd97a85e1707f9ee78709af855d6258
Stephen Gary <sgary0@protonmail.com>
2020-09-17 20:04:49 -0400

    migrations seeds dbUsers

--- knexfile.js
+++ knexfile.js
@@ -4,27 +4,27 @@ module.exports = {
   development: {
     client: "sqlite3",
     connection: {
-      filename: "./data/hobbits.db3",
+      filename: "./database/users.db3",
     },
     useNullAsDefault: true,
     migrations: {
-      directory: "./data/migrations",
+      directory: "./database/migrations",
     },
     seeds: {
-      directory: "./data/seeds",
+      directory: "./database/seeds",
     },
   },
   testing: {
     client: "sqlite3",
     connection: {
-      filename: "./data/test.db3",
+      filename: "./database/test.db3",
     },
     useNullAsDefault: true,
     migrations: {
-      directory: "./data/migrations",
+      directory: "./database/migrations",
     },
     seeds: {
-      directory: "./data/seeds",
+      directory: "./database/seeds",
     },
   },
 
@@ -36,10 +36,10 @@ module.exports = {
       max: 10,
     },
     migrations: {
-      directory: "./data/migrations",
+      directory: "./database/migrations",
     },
     seeds: {
-      directory: "./data/seeds",
+      directory: "./database/seeds",
     },
   },
 };
